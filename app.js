b602180cb439875605ba425ae116c8d4bbbd64db
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-06-11 11:22:34 +0700

    feat: add usersRouter and cardsRouter to app.js

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/mydb');
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
