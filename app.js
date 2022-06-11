6a4f274ad77e961dbcd36c5f5fd0e46a95bc77d2
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-06-11 10:18:46 +0700

    feat: add bodyParser to app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/mydb');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
