657d11b4ef8b982172dc8f015a2c8e50b1a175c5
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-06-12 19:40:45 +0700

    feat: add controllers for users and cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,47 +1,80 @@
 const Card = require('../models/card');
+const { isDbErrors } = require('../middlewares/app');
 
 const getAllCards = (req, res) => {
   Card.find({})
     .then((cards) => {
-      res.send({data: cards})
-    })
+      res.send({ cards });
+    }).catch((err) => {
+      isDbErrors(res, err);
+  });
 };
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
-  Card.create({name, link, owner: req.user._id})
+  Card.create({ name, link, owner: req.user._id })
     .then((card) => {
-      res.send({data: card})
-    })
-}
+      res.send({
+        name: card.name,
+        link: card.link,
+        owner: card.owner,
+        likes: card.likes,
+        createdAt: card.createdAt,
+        _id: card._id
+      });
+    }).catch((err) => {
+      isDbErrors(res, err);
+  });
+};
 
 const deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.id)
+  Card.findByIdAndRemove(req.params.cardId)
     .then(() => {
-      res.send({message: 'Card was deleted'})
-    })
-}
+      res.send(
+        { message: 'Card was deleted' }
+      );
+    }).catch((err) => {
+      isDbErrors(res, err);
+  });
+};
 
 const addCardLike = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     ({ $addToSet: { likes: req.user._id } }),
-    { new: true, runValidators: true }
-  ).then((likes) => {
-      res.send({data: likes})
-    })
-}
+    { new: true, runValidators: true })
+    .then((card) => {
+      res.send({
+        name: card.name,
+        link: card.link,
+        owner: card.owner,
+        likes: card.likes,
+        createdAt: card.createdAt,
+        _id: card._id
+      });
+    }).catch((err) => {
+      isDbErrors(res, err);
+  });
+};
 
 const deleteCardLike = (req, res) => {
   Card.findByIdAndUpdate(
-    req.params.id,
+    req.params.cardId,
     { $pull: { likes: req.user._id } },
-    { new: true }
-  ).then((likes) => {
-      res.send({data: likes})
-    })
-}
-
+    { new: true })
+    .then((card) => {
+      res.send({
+        name: card.name,
+        link: card.link,
+        owner: card.owner,
+        likes: card.likes,
+        createdAt: card.createdAt,
+        _id: card._id
+      });
+    }).catch((err) => {
+      isDbErrors(res, err);
+  });
+};
 
 module.exports = {
   getAllCards,
@@ -49,4 +82,4 @@ module.exports = {
   deleteCard,
   addCardLike,
   deleteCardLike
-}
+};
