3dadfd514d392f2b4589bfb28be08c08d8f76b1a
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-06-13 08:05:07 +0700

    fix: add validation message to userSchema

--- models/user.js
+++ models/user.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30
+    required: [true, 'Поле с именем обязательно для заполнения. Получена пустая строка.'],
+    minlength: [2, 'Имя должно содержать минимум 2 знака. Получена строка: {VALUE}'],
+    maxlength: [30, 'Имя должно содержать максимум 30 знаков. Получена строка: {VALUE}']
   },
   about: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30
+    required: [true, 'Поле с описанием рода деятельности обязательно для заполнения. Получена пустая строка.'],
+    minlength: [2, 'Описание рода деятельности должно содержать минимум 2 знака. Получена строка: {VALUE}'],
+    maxlength: [30, 'Описание рода деятельности должно содержать максимум 30 знаков. Получена строка: {VALUE}']
   },
   avatar: {
     type: String,
-    required: true
+    required: [true, 'Поле со ссылкой на аватар обязательно для заполнения. Получена пустая строка.']
   }
 });
 
