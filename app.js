24e0128d126d7acd3584ae5d3d782373c0e54774
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-07-07 16:05:41 +0700

    fix: add routes for sign-in ans sign-up

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const { login, createUser } = require('./controllers/users');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { isNotResource } = require('./middlewares/app');
@@ -17,6 +18,9 @@ app.use((req, res, next) => {
   req.user = { _id: '62a4175a3d9d8374d7f89724' };
   next();
 });
+
+app.post('/signin', login);
+app.post('/signup', createUser);
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
