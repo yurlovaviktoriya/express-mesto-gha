2097894c2280acaef44d66329601ce75a1e511c8
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-07-15 19:43:05 +0700

    feat: add logging to app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { isNotResource, sendResponseWithErrorMessage } = require('./middlewares/app');
 const { signupValidator, signinValidator } = require('./middlewares/validation/users');
+const { requestLogger, errorLogger } = require('./middlewares/logging/logger');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
@@ -19,6 +20,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.post('/signin', signinValidator, login);
 app.post('/signup', signupValidator, createUser);
 
@@ -27,6 +30,8 @@ app.use(auth);
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 app.use('*', isNotResource);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(sendResponseWithErrorMessage);
